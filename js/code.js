a662467e0a54824004e9f630bd6079d62f3ea9be
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-10-19 14:19:45 -0400

    Finished all minus the mad libs function so far.

--- js/code.js
+++ js/code.js
@@ -16,19 +16,25 @@
      // TODO 1: Declare and instantiate a variable called `firstName` to store
      // your first name.
      // Also DECLARE (but do not instantiate) a `lastName` variable.
+      var firstName = "Rodolfo";
+      var lastName;
 
-     
 
      // TODO 2: Create a `greeting` variable as a string which says:
      //    "Hello there _____!"
      // Replace the _____ above with the value of the `firstName` variable.
      // NOTE: Do NOT simply retype your first name in the `greeting` value.
 
-
+      var greeting = "Hello there";
+          greeting = "Hello there " + firstName + "!";
 
      // TODO 3: Create two variables: one for the year you were born, and one
      // for the current year. Then create a third variable to store your age!
      // Name these: `birthYear`, `currentYear`, and `age`
+
+     var birthYear = 1995;
+     var currentYear = 2016;
+     var age = currentYear - birthYear;
 
 
 
@@ -36,7 +42,15 @@
      // store "hip" in this variable, but if your age is 35 or over, store
      // "classic" in this variable.
 
-
+     var style;
+      if (age < 35) {
+        console.log("hip");
+        style = "hip";
+      }
+      else {
+        console.log("classic");
+        style = "classic";
+      }
 
      // TODO 5: Create a variable indicating that your instructor is awesome
      // and call it `instructorIsAwesome`. This variable should be a boolean
@@ -44,12 +58,24 @@
      // Next, create a variable called `review` and, if the instructor is
      // awesome, set `review` to "good", otherwise set the `review` variable to "bad"
 
-
+     var instructorIsAwesome = true;
+      var review;
+        if (instructorIsAwesome === true) {
+          review = "good";
+        }
+        else {
+          review = "bad";
+        }
 
      // TODO 6: Create a function called `math` which accepts to numbers as
      // arguments and returns the result of adding them together, mutliplying
      // by 10, then dividing by the first number.
 
+     function math (x, y) {
+
+       return (x + y) * 10 / x
+
+     }
 
 
      // TODO 7: Write a function called `madlib` that takes 3 separate words as
@@ -57,8 +83,12 @@
      // "I prefer __1__ while I __2__ so that I don't __3__ on the __4__."
      // Finally, the function should return that new sentence.
 
+     function madlib (w, x, y, z) {
 
 
+       var log = "I prefer " + w + " while I " + x + " so that I don/'t " + y + " on the " + z + ".";
+
+     }
      /* ***************************************************************
                               EPIC Mode
 
